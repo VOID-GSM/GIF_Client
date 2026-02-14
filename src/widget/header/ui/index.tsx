8fa80bc983eeef98e31c8b1c6d8d8ee001246305
jyuuuuu0 <s25023@gsm.hs.kr>
2026-02-14 18:52:00 +0900

    refactor: border 생상을 globals.css에 정의된 색상으로 변경

--- src/widget/header/ui/index.tsx
+++ src/widget/header/ui/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="h-20 w-full flex justify-center items-center border-b border-[#B8B8B8] bg-white">
+    <header className="h-20 w-full flex justify-center items-center border-b border-gray-70 bg-white">
       <Link href="/">
         <Image src={Logo} alt="GIF LOGO" width={61} height={56} priority />
       </Link>
