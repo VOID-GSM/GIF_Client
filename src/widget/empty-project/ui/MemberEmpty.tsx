2cea3148ecf88b4cedbcd62903fa3bb1bb32bf5b
jyuuuuu0 <s25023@gsm.hs.kr>
2026-02-27 04:44:42 +0900

    feat: (팀원) 프로젝트 생성 전 메인

---
+++ src/widget/empty-project/ui/MemberEmpty.tsx
@@ -0,0 +1,7 @@
+export default function MemberEmpty() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p className="font-medium text-2xl text-gray-40">아직 참여한 프로젝트가 없습니다.</p>
+    </div>
+  );
+}
