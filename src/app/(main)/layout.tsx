9ca08ffe093ba19a0bfd122fab5f477489a72d01
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-04 23:20:32 +0900

    refactor: 인라인 스타일 Tailwind CSS로 변경

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,10 +4,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Header />
-      <main
-        className="flex flex-col items-center justify-center"
-        style={{ height: 'calc(100vh - 80px)' }}
-      >
+      <main className="flex flex-col items-center justify-center h-[calc(100vh-80px)]">
         {children}
       </main>
     </>
