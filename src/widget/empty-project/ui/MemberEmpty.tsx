85625a31dbcbe6c999267e1a1b91e0e6bacd8a31
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-04 23:24:46 +0900

    refactor: 임의 값을 Tailwind CSS 기본 스페이싱 단위로 변경

--- src/widget/empty-project/ui/MemberEmpty.tsx
+++ src/widget/empty-project/ui/MemberEmpty.tsx
@@ -1,6 +1,6 @@
 export default function MemberEmpty() {
   return (
-    <div className="flex flex-col items-center justify-center gap-[6px]">
+    <div className="flex flex-col items-center justify-center gap-1.5">
       <p className="font-medium text-2xl text-gray-40">아직 참여한 프로젝트가 없습니다.</p>
       <p className="font-medium text-gray-60">팀장이 프로젝트를 생성하면 자동으로 참여됩니다</p>
     </div>
