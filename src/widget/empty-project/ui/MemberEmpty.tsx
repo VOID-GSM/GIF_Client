81597c114b20520e8f47797a99c2101d94791f0a
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-04 15:16:21 +0900

    refactor: 문구 추가

--- src/widget/empty-project/ui/MemberEmpty.tsx
+++ src/widget/empty-project/ui/MemberEmpty.tsx
@@ -1,7 +1,8 @@
 export default function MemberEmpty() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center gap-[6px]">
       <p className="font-medium text-2xl text-gray-40">아직 참여한 프로젝트가 없습니다.</p>
+      <p className="font-medium text-gray-60">팀장이 프로젝트를 생성하면 자동으로 참여됩니다</p>
     </div>
   );
 }
