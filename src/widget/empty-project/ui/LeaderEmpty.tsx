fb243f32dd568a2f8229937acaef4903bbe785d0
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-04 15:15:59 +0900

    design: gap 수정

--- src/widget/empty-project/ui/LeaderEmpty.tsx
+++ src/widget/empty-project/ui/LeaderEmpty.tsx
@@ -2,7 +2,7 @@ import CreateProjectButton from '@/features/create-project/ui/CreateProjectButto
 
 export default function LeaderEmpty() {
   return (
-    <div className="flex flex-col items-center justify-center gap-6">
+    <div className="flex flex-col items-center justify-center gap-[20px]">
       <p className="font-medium text-2xl text-gray-40">아직 참여한 프로젝트가 없습니다.</p>
       <CreateProjectButton />
     </div>
