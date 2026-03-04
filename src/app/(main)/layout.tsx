d8f191f24905fdb7854e91c2df363eec79c13966
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-04 15:16:45 +0900

    refactor: layout 가운데 정렬

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,12 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <Header />
-      <main className="min-h-screen">{children}</main>
+      <main
+        className="flex flex-col items-center justify-center"
+        style={{ height: 'calc(100vh - 80px)' }}
+      >
+        {children}
+      </main>
     </>
   );
 }
