e98c4dfc0e361dcc16d1e4b46dd64bd614bb79ef
ma94275 <s25020@gsm.hs.kr>
2026-03-05 20:34:37 +0900

    feat: ProjectButton 컴포넌트 설정

---
+++ src/shared/ui/ProjectButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+interface ProjectButtonProps {
+  text: string;
+  disabled: boolean; 
+  onClick?: () => void;
+}
+
+export default function ProjectButton({ text, disabled, onClick }: ProjectButtonProps) {
+  return(
+    <div>
+      <button type="submit"
+        onClick={onClick}
+        disabled={disabled}
+        className={`w-100 h-10 rounded-[10px] text-white font-medium text-[20px]
+          ${disabled ? "bg-[#e6e6e6]" : "bg-[#31C690] cursor-pointer"}`}
+      >
+        {text}
+      </button>
+    </div>
+  )
+}
