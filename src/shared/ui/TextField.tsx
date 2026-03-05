edefa57ae5177c5719a32266511d7ebb3a46279b
ma94275 <s25020@gsm.hs.kr>
2026-03-05 23:58:10 +0900

    refactor: 불필요한 태그 제거

--- src/shared/ui/TextField.tsx
+++ src/shared/ui/TextField.tsx
@@ -6,14 +6,10 @@ interface TextFieldProps {
 
 export default function TextField({ placeholder, value, onChange }: TextFieldProps) {
   return (
-    <div>
-      <div className="w-[400px] h-[50px]">
-        <input type="text"
-        value={value}
-        onChange={onChange}
-        className="w-full h-full border border-[#bcbcbc] px-[15px] py-3 rounded-[10px] focus:outline-none focus:border-[#000000] text-[18px] font-medium placeholder-[#8b8b8b]" 
-        placeholder={placeholder}/>
-      </div>
-    </div>
+    <input type="text"
+    value={value}
+    onChange={onChange}
+    className="w-full h-[50px] border border-[#bcbcbc] px-[15px] py-3 rounded-[10px] focus:outline-none focus:border-[#000000] text-[18px] font-medium placeholder-[#8b8b8b]" 
+    placeholder={placeholder}/>
   )
 }
