b6d53822281139152aa77aac07e015481c78ef05
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-05 10:20:02 +0900

    feat: 구글 로그인 버튼 컴포넌트 생성

---
+++ src/widget/signin/ui/GoogleLoginButton.tsx
@@ -0,0 +1,10 @@
+import GoogleLogo from '@/shared/asset/svg/GoogleLogo';
+
+export default function GoogleLoginBUtton() {
+  return (
+    <div className="w-100 h-12.5 flex items-center border border-gray-60 rounded-[10px] py-2 pl-5 gap-15.5">
+      <GoogleLogo />
+      <p className="font-medium text-2xl">Google로 시작하기</p>
+    </div>
+  );
+}
