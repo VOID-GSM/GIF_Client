8158ef34a098e62d3aa2bd20b98a42ed82dc5cf0
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-05 10:26:45 +0900

    refactor: 버튼 요소 div 태그에서 button 태그로 변경

--- src/widget/signin/ui/GoogleLoginButton.tsx
+++ src/widget/signin/ui/GoogleLoginButton.tsx
@@ -2,9 +2,9 @@ import GoogleLogo from '@/shared/asset/svg/GoogleLogo';
 
 export default function GoogleLoginButton() {
   return (
-    <div className="w-100 h-12.5 flex items-center border border-gray-60 rounded-[10px] py-2 pl-5 gap-15.5">
+    <button className="w-100 h-12.5 flex items-center border border-gray-60 rounded-[10px] py-2 pl-5 gap-15.5">
       <GoogleLogo />
       <p className="font-medium text-2xl">Google로 시작하기</p>
-    </div>
+    </button>
   );
 }
