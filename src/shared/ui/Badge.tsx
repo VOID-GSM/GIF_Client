bb74bee6d98357d9650a33286814a3d9e87cfb88
ma94275 <s25020@gsm.hs.kr>
2026-03-05 20:34:11 +0900

    faet: Bedge 컴포넌트 설정

---
+++ src/shared/ui/Badge.tsx
@@ -0,0 +1,24 @@
+import Deleted from "../asset/svg/Deleted";
+
+interface BadgeProps {
+  id: string;
+  name: string;
+  onRemove: (id: string) => void; 
+}
+
+export default function Badge({ id, name, onRemove }: BadgeProps) {
+  return (
+    <div className="w-auto px-3 h-[30px] bg-[#ededed] rounded-[20px] flex items-center justify-center gap-2">
+      <span className="font-medium text-[14px]">{id} {name}</span>
+      <button 
+        type="button"
+        onClick={(e) => {
+          e.stopPropagation(); 
+          onRemove(id);
+        }}
+      >
+        <Deleted />
+      </button>
+    </div>
+  );
+}
