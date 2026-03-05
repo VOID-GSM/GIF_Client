9960563ca9a73daff715c696fd5a5b4cec3e08fc
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-05 10:20:20 +0900

    feat: 로그인 페이지 생성

---
+++ src/view/signin/ui/SigninView.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import Logo from '@/shared/asset/img/gif_logo.png';
+import GoogleLoginBUtton from '@/widget/signin/ui/GoogleLoginButton';
+
+export default function SigninView() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-[#F7FFFC]">
+      <div className="w-125 h-75 flex flex-col items-center gap-19 bg-white border border-gray-80 rounded-[10px] px-12.5 pb-13.5 pt-19">
+        <Image src={Logo} alt="GIF LOGO" width={100} height={66} priority />
+        <GoogleLoginBUtton />
+      </div>
+    </div>
+  );
+}
