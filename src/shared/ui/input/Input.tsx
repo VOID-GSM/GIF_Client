f356c625f04d8594f4a7c0925f460a2ebdbe8b3a
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-06 00:35:40 +0900

    feat: input 컴포넌트 value props 추가

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -3,15 +3,17 @@ import { ChangeEvent } from 'react';
 interface InputProps {
   placeholder?: string;
   type?: string;
+  value?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   className?: string;
 }
 
-export default function Input({ placeholder, type = 'text', onChange }: InputProps) {
+export default function Input({ placeholder, type = 'text', value, onChange }: InputProps) {
   return (
     <input
       type={type}
       placeholder={placeholder}
+      value={value}
       onChange={onChange}
       className="w-100 h-12.5 bg-white border border-[#787878] rounded-[10px] py-3 px-3.75 font-medium text-lg text-black placeholder:text-[#8B8B8B] focus:outline-none"
     />
