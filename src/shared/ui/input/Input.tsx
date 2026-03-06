258bb131fa4519d29436ae71b0d213b534d73f00
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-06 14:05:18 +0900

    chore: Input 컴포넌트 className props 추가

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -8,14 +8,20 @@ interface InputProps {
   className?: string;
 }
 
-export default function Input({ placeholder, type = 'text', value, onChange }: InputProps) {
+export default function Input({
+  placeholder,
+  type = 'text',
+  value,
+  onChange,
+  className,
+}: InputProps) {
   return (
     <input
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      className="w-100 h-12.5 bg-white border border-[#787878] rounded-[10px] py-3 px-3.75 font-medium text-lg text-black placeholder:text-[#8B8B8B] focus:outline-none"
+      className={`w-100 h-12.5 bg-white border border-[#787878] rounded-[10px] py-3 px-3.75 font-medium text-lg text-black placeholder:text-[#8B8B8B] focus:outline-none ${className}`}
     />
   );
 }
