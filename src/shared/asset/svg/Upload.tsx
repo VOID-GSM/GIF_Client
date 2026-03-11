a6cbcb31caa7d7b9a259537347c17eeac0e45e73
leehyoeun <s25049@gsm.hs.kr>
2026-03-11 13:48:53 +0900

    feat: form 페이지 svg 추가

---
+++ src/shared/asset/svg/Upload.tsx
@@ -0,0 +1,27 @@
+export function Upload() {
+  return (
+    <svg 
+      width="44"
+      height="43"
+      viewBox="0 0 44 43"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.5 29.8335V41.5001H41.7053V29.8335"
+        stroke="#969696"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M22.2025 29.8333V1.5M22.2025 1.5L11.7857 11.9054M22.2025 1.5L32.6192 11.9054"
+        stroke="#969696"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
