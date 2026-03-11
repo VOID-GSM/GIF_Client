ce356bf50587e00c8ae5f9e28aebb51c53ed2610
leehyoeun <s25049@gsm.hs.kr>
2026-03-11 13:48:09 +0900

    feat: Cancle svg 추가

---
+++ src/shared/asset/svg/Cancle.tsx
@@ -0,0 +1,24 @@
+export function Cancle() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 55 55" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path 
+        d="M3.51685 2.5L52.5 52.4997" 
+        stroke="black" 
+        strokeWidth="5" 
+        strokeLinecap="round"
+      />
+      <path 
+        d="M51.4832 2.5L2.49998 52.4997" 
+        stroke="black" 
+        strokeWidth="5" 
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
