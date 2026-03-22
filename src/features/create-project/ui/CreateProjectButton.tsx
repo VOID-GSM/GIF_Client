c4458863a66a45dffeda88a156da3412534fc2ec
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-22 17:32:00 +0900

    fix: 프로젝트 생성 버튼 이동 경로 변경

--- src/features/create-project/ui/CreateProjectButton.tsx
+++ src/features/create-project/ui/CreateProjectButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function CreateProjectButton() {
   return (
     <Link
-      href="/project/create"
+      href="/projectSetup"
       className="flex justify-center items-center w-[25rem] h-[45px] bg-main text-white font-semibold text-2xl rounded-[10px]"
     >
       프로젝트 생성하기
