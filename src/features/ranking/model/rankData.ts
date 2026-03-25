67af5667dfae0a13165c6099ba15e512cac5932e
leehyoeun <s25049@gsm.hs.kr>
2026-03-25 17:44:10 +0900

    feat: rankItem 타입 및 랭킹 데이터 추가

---
+++ src/features/ranking/model/rankData.ts
@@ -0,0 +1,14 @@
+import { RankItem } from './type';
+
+export const GRADE_1_RANKING: RankItem[] = [
+  { name: '깨소금', rank: 1},
+  { name: '후추', rank: 2},
+  { name: '소금', rank: 3},
+  { name: '간장', rank: 4},
+]
+
+export const GRADE_2_RANKING: RankItem[] = [
+  { name: 'VOID', rank: 1},
+  { name: '책사모', rank: 2},
+  { name: '맛소금', rank: 3},
+]
