e9ce3d7546dfc3dba58cd46fda8286c7c63bc7ec
leehyoeun <s25049@gsm.hs.kr>
2026-03-25 17:51:36 +0900

    fix: 등수 확인 버튼 오픈 날짜 수정

--- src/view/main/ui/MainView.tsx
+++ src/view/main/ui/MainView.tsx
@@ -14,8 +14,7 @@ interface Props {
   currentUserId: string;
 }
 
-const TARGET_DATE = new Date('2022-12-29T00:00:00');
-
+const TARGET_DATE = new Date('2026-12-29T00:00:00');
 export default function MainView({ project: initial, currentUserId }: Props) {
   const [project, setProject] = useState(initial);
   const [isModalOpen, setIsModalOpen] = useState(false);
